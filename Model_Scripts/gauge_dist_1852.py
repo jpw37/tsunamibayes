4f16eb674ec746daa39b8db784f831016d55e2a0
Codac <codac@live.com>
2018-09-18 10:20:29 -0600

    Fixed Loading Of Guages

--- Model_Scripts/gauge_dist_1852.py
+++ Model_Scripts/gauge_dist_1852.py
@@ -17,7 +17,7 @@ def load_gauges():
     name = 10001 # Pulu Ai - Wichmann
     longitude = -4.517863
     latitude = 129.7745653
-    distance = 0 # in kilometers (max 5) //TODO Not sure what this is??
+    distance = 2.5 # in kilometers (max 5) //TODO Not sure what this is??
     kind = [None, 'norm', None]
 
     # For kind = 'norm'
@@ -42,9 +42,9 @@ def load_gauges():
 #GAUGE 2_____________Water Height______________
     city_name = 'Ambonia - Wichmann'
     name = 10002
-    longitude = -3.76
-    latitude = 128.18
-    distance = 7.41 # in kilometers (max 5) //TODO Not sure what this is??
+    longitude = -3.693521
+    latitude = 128.175538
+    distance = 2.5 # in kilometers (max 5) //TODO Not sure what this is??
     kind = [None, 'norm', None]
 
     # For kind = 'norm'
@@ -67,23 +67,32 @@ def load_gauges():
 # GAUGE 3___________Water Height________________
     city_name = 'Banda Neira - Tsunami Catalog'
     name = 10003  #
-    longitude = -4.5
-    latitude = 129.85
-    distance = 6.216  # in kilometers (max 5) //TODO Not sure what this is??
-    kind = [None, 'norm', None]
+    longitude = -4.529905
+    latitude = 129.897376
+    distance = 2.5  # in kilometers (max 5) //TODO Not sure what this is??
+    kind = ['skewnorm', 'norm', 'skewnorm']
 
     # For kind = 'norm'
-    arrival_mean = None  # in minutes
-    arrival_std = None
-    arrival_params = [arrival_mean, arrival_std]
+    arrival_skew_param = 5
+    arrival_mean = 15  # in minutes
+    arrival_std = 10
+    arrival_params = [arrival_skew_param, arrival_mean, arrival_std]
     height_mean = 4.0  # in meters
     height_std = 1.0
     height_params = [height_mean, height_std]
-    inundation_std = None
-    inundation_mean = None
-    inundation_params = [inundation_mean, inundation_std]
-    beta = 0
-    n = .015
+    inundation_skew_param = 3
+    inundation_mean = 231  # in meters
+    inundation_std = 85
+    inundation_params = [inundation_skew_param, inundation_mean, inundation_std]
+
+    # Load the beta angles for the shore elevation for inundation priors for
+    # BANDA_NEIRA
+    ShoreAngles = ShoreAngle.ShoreLineAngles('south_banda_neira_profiles')
+    profiles_to_average = [1, 2, 3]
+    BANDA_NEIRA_average_angle = ShoreAngles.getAveragesSlopeAngles(profiles_to_average)
+    beta = BANDA_NEIRA_average_angle
+
+    n = .03
 
     g = Gauge(name, longitude, latitude, distance,
                     kind, arrival_params, height_params, inundation_params, beta, n, city_name)
@@ -92,9 +101,9 @@ def load_gauges():
 # GAUGE 4____________Water Height_______________
     city_name = 'PULAU BURU - Wichmann Catalog'
     name = 10004  #
-    longitude = -3.85085187
-    latitude = 126.732576
-    distance = 0.25  # in kilometers (max 5) //TODO Not sure what this is??
+    longitude = -3.848928
+    latitude = 126.733678
+    distance = 2.5  # in kilometers (max 5) //TODO Not sure what this is??
     kind = [None, 'chi2', None]
 
     # For kind = 'chi2'
@@ -118,10 +127,10 @@ def load_gauges():
 # GAUGE 5___________Water Height________________
     city_name = 'Saparua - Wichmann Catalog'
     name = 10005  #
-    longitude = -3.6
-    latitude = 128.68
-    distance = 3.559  # in kilometers (max 5) //TODO Not sure what this is??
-    kind = [None, 'norm', None]
+    longitude = -3.576063
+    latitude = 128.658715
+    distance = 2.5  # in kilometers (max 5) //TODO Not sure what this is??
+    kind = [None, 'norm', 'norm']
 
     # For kind = 'norm'
     arrival_mean = None  # in minutes
@@ -130,90 +139,30 @@ def load_gauges():
     height_mean = 3.0  # in meters
     height_std = .75
     height_params = [height_mean, height_std]
-    inundation_std = None
-    inundation_mean = None
-    inundation_params = [inundation_mean, inundation_std]
-    beta = 0
-    n = .03
-
-    g = Gauge(name, longitude, latitude, distance,
-                    kind, arrival_params, height_params, inundation_params, beta, n, city_name)
-    gauges.append(g.to_json())
-
-# GAUGE 6___________Inundation________________
-    city_name = 'Suparua - Wichmann'
-    name = 10006  # Suparua - Wichmann
-    longitude = -3.6
-    latitude = 128.68
-    distance = 3.559  # in kilometers (max 5) //TODO Not sure what this is??
-    kind = [None, None, 'norm']
-
-    # For kind = 'norm'
-    arrival_mean = None  # in minutes
-    arrival_std = None
-    arrival_params = [arrival_mean, arrival_std]
-    height_mean = None  # in meters
-    height_std = None
-    height_params = [height_mean, height_std]
-    inundation_mean = 123.44
-    inundation_std = 5. #.185806 was the value given by Hunter's reconstruction
+    inundation_std = 123.44
+    inundation_mean = .185806
     inundation_params = [inundation_mean, inundation_std]
 
-    #Load the beta angles for the shore elevation for inundtion priors for
+    # Load the beta angles for the shore elevation for inundtion priors for
     # SUPARUA
     ShoreAngles = ShoreAngle.ShoreLineAngles('suparua_profiles')
-    profiles_to_average = [4,5,8,9,10]
+    profiles_to_average = [4, 5, 8, 9, 10]
     SUPARUA_average_angle = ShoreAngles.getAveragesSlopeAngles(profiles_to_average)
     beta = SUPARUA_average_angle
 
     n = .03
 
     g = Gauge(name, longitude, latitude, distance,
-              kind, arrival_params, height_params, inundation_params, beta, n, city_name)
+                    kind, arrival_params, height_params, inundation_params, beta, n, city_name)
     gauges.append(g.to_json())
 
 
-# GAUGE 7___________Inundation_________________
-    city_name = 'Banda Neira - Wichmann'
-    name = 10007  #
-    longitude = -4.5
-    latitude = 129.85
-    distance = 6.216  # in kilometers (max 5) //TODO Not sure what this is??
-    kind = [None, None, 'skewnorm']
-
-    # For kind = 'skewnorm'
-    arrival_skew_param = None
-    arrival_mean = None  # in minutes
-    arrival_std = None
-    arrival_params = [arrival_skew_param, arrival_mean, arrival_std]
-    height_skew_param = None
-    height_mean = None  # in meters
-    height_std = None
-    height_params = [height_skew_param, height_mean, height_std]
-    inundation_skew_param = 3
-    inundation_mean = 231  # in meters
-    inundation_std = 85
-    inundation_params = [inundation_skew_param, inundation_mean, inundation_std]
-
-    # Load the beta angles for the shore elevation for inundtion priors for
-    # BANDA_NEIRA
-    ShoreAngles = ShoreAngle.ShoreLineAngles('south_banda_neira_profiles')
-    profiles_to_average = [1,2,3]
-    BANDA_NEIRA_average_angle = ShoreAngles.getAveragesSlopeAngles(profiles_to_average)
-    beta = BANDA_NEIRA_average_angle
-
-    n = .03
-
-    g = Gauge(name, longitude, latitude, distance,
-              kind, arrival_params, height_params, inundation_params, beta, n, city_name)
-    gauges.append(g.to_json())
-
-# GAUGE 8___________Inundation_________________
+# GAUGE 6___________Inundation_________________
     city_name = 'Lonthor - Wichmann'
-    name = 10008  #
-    longitude = -4.5
-    latitude = 129.85
-    distance = 5.989  # in kilometers (max 5) //TODO Not sure what this is??
+    name = 10006  #
+    longitude = -3.576063
+    latitude = 128.658715
+    distance = 2.5  # in kilometers (max 5) //TODO Not sure what this is??
     kind = [None, None, 'skewnorm']
 
 
@@ -245,43 +194,12 @@ def load_gauges():
     gauges.append(g.to_json())
 
 
-# GAUGE 9___________Arrival Time_________________
-    city_name = 'Banda Neira  - Wichmann'
-    name = 10009
-    longitude = -4.5
-    latitude = 129.85
-    distance = 6.216  # in kilometers (max 5) //TODO Not sure what this is??
-    kind = ['skewnorm', None, None]
-
-
-    # For kind = 'skewnorm'
-    arrival_skew_param = 5
-    arrival_mean = 15  # in minutes
-    arrival_std = 10
-    arrival_params = [arrival_skew_param, arrival_mean, arrival_std]
-    height_skew_param = None
-    height_mean = None  # in meters
-    height_std = None
-    height_params = [height_skew_param, height_mean, height_std]
-    inundation_skew_param = None
-    inundation_mean = None  # in meters
-    inundation_std = None
-    inundation_params = [inundation_skew_param, inundation_mean, inundation_std]
-
-    beta = 0
-    n = .03
-
-    g = Gauge(name, longitude, latitude, distance,
-              kind, arrival_params, height_params, inundation_params, beta, n, city_name)
-    gauges.append(g.to_json())
-
-
-# GAUGE 10___________Arrival Time________________
+# GAUGE 7___________Arrival Time________________
     city_name = 'Pulau Saparua  - Wichmann'
-    name = 10010
-    longitude = -3.6
-    latitude = 128.68
-    distance = 3.559  # in kilometers (max 5) //TODO Not sure what this is??
+    name = 10007
+    longitude = -3.576063
+    latitude = 128.658715
+    distance = 2.5  # in kilometers (max 5) //TODO Not sure what this is??
     kind = ['skewnorm', None, None]
 
     # For kind = 'skewnorm'
@@ -305,7 +223,7 @@ def load_gauges():
     gauges.append(g.to_json())
 
 
-# GAUGE 11___________Wave Height________________
+# GAUGE 8___________Wave Height________________
     # ---------------------------------------------------------------------
     # THIS IS THE AMBONIA OBSERVATION THAT WE ARE OMITTING FROM THIS RUN.
     # ---------------------------------------------------------------------
