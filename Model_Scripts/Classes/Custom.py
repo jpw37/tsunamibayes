36c7f939cacc9e5c59e66d14714177c5f040bdda
Codac <codac@live.com>
2018-10-31 23:14:12 -0600

    Fixed File path

--- Model_Scripts/Classes/Custom.py
+++ Model_Scripts/Classes/Custom.py
@@ -34,12 +34,12 @@ class Custom(MCMC):
         samplingMult = 50
         bandwidthScalar = 2
         # build longitude, latitude and strike prior
-        data = pd.read_excel('../Data/Fixed92kmFaultOffset50kmgapPts.xls')
+        data = pd.read_excel('./../Data/Fixed92kmFaultOffset50kmgapPts.xls')
         data = np.array(data[['POINT_X', 'POINT_Y', 'Strike']])
         distrb0 = gaussian_kde(data.T)
 
         # build dip, rake, depth, length, width, and slip prior
-        vals = np.load('../Data/6_param_bootstrapped_data.npy')
+        vals = np.load('./../Data/6_param_bootstrapped_data.npy')
         distrb1 = gaussian_kde(vals.T)
         distrb1.set_bandwidth(bw_method=distrb1.factor * bandwidthScalar)
 
