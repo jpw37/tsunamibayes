f9e85fe49a9edbbd63b6e56af35bc3acf58cf703
Codac <codac@live.com>
2018-11-09 01:05:39 -0700

    Testing

--- Model_Scripts/Classes/Samples.py
+++ Model_Scripts/Classes/Samples.py
@@ -106,6 +106,7 @@ class Samples:
             saves = saves.tolist()
         print("------------------SAVING ------------\n", saves)
         self.okada.loc[len(self.okada)] = saves
+        print("SAVED AS", self.okada.loc[len(self.okada)])
 
     def get_sample_okada(self):
         """
