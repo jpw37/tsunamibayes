89435dba413d63b3a7d30fbd2b2fb3b8673c0d3c
Codac <codac@live.com>
2018-11-09 00:37:43 -0700

    Changed typos and added the saving of initial okada sample parameters

--- Model_Scripts/Classes/Scenario.py
+++ Model_Scripts/Classes/Scenario.py
@@ -27,6 +27,12 @@ class Scenario:
     def __init__(self, title="Default_Title", use_custom=False, init='manual', rw_covariance=1.0, method="random_walk", iterations=1):
         """
         Initialize all the correct variables for Running this Scenario
+        :param title: Title for Scinerio (ex: 1852)
+        :param use_custom: Bool: To use the custom methods for MCMC or not
+        :param init: String: (manual, random or restart) How to initialize the parameters
+        :param rw_covariance: float: covariance for the random walk method
+        :param method: String: MCMC Method to use
+        :param iterations: Int: Number of Times to run the model
         """
         # Clear previous files
         os.system('rm ./Data/Topo/dtopo.tt3')
@@ -39,6 +45,7 @@ class Scenario:
         self.init = init
         self.feedForward = FeedForward()
 
+        # Set the MCMC class based on input
         if(use_custom):
             self.mcmc = Custom()
         elif(method == "independent_sampler"):
@@ -46,12 +53,23 @@ class Scenario:
         else:
             self.mcmc = RandomWalk(rw_covariance)
 
+        # Initialize the Samples Class
         self.samples = Samples(title, self.mcmc.sample_cols, self.mcmc.proposal_cols)
 
         self.mcmc.set_samples(self.samples)
+
+        # Get initial draw for the initial run of geoclaw
         self.init_guesses = self.mcmc.init_guesses(self.init)
+        # Save the samples
         self.samples.save_sample(self.init_guesses)
 
+        # If using the custom methods map the initial guesses to okada parameters to save as initial sample
+        if (self.use_custom):
+            self.init_guesses = self.mcmc.map_to_okada(self.init_guesses)
+        # Save
+        self.samples.save_sample_okada(self.init_guesses)
+
+        # Build the prior for the model, based on the choice of MCMC
         self.prior = self.mcmc.build_priors()
 
         # Make sure Pre-Run files have been generated
@@ -88,27 +106,26 @@ class Scenario:
     def run(self):
         """
         Runs the Scenario For the given amount of iterations
-        :return:
         """
         for i in range(self.iterations):
 
             # Get current Sample and draw a proposal sample from it
             sample_params = self.samples.get_sample()
-            proposed_params = self.mcmc.draw(sample_params)
+            proposal_params = self.mcmc.draw(sample_params)
 
             # Save the proposal draw for debugging purposes
-            self.samples.save_proposal(proposed_params)
+            self.samples.save_proposal(proposal_params)
 
             # If instructed to use the custom parameters, map parameters to Okada space (9 Dimensional)
             if(self.use_custom):
-                proposed_params = self.mcmc.map_to_okada(proposed_params)
+                proposal_params = self.mcmc.map_to_okada(proposal_params)
 
             # Save Proposal
-            self.samples.save_proposal_okada(proposed_params)
-            proposed_params = self.samples.get_proposal_okada()
+            self.samples.save_proposal_okada(proposal_params)
+            proposal_params = self.samples.get_proposal_okada()
 
             # Run Geo Claw on the new proposal
-            self.feedForward.run_geo_claw(proposed_params)
+            self.feedForward.run_geo_claw(proposal_params)
 
             # Calculate the Log Likelihood probability for the new draw
             proposal_llh = self.feedForward.calculate_llh(self.gauges)
@@ -118,7 +135,7 @@ class Scenario:
             self.samples.save_proposal_llh(proposal_llh)
 
             # Calculate prior probability for the current sample and proposed sample
-            sample_prior_llh, proposal_prior_llh = self.prior.logpdf(sample_params, proposed_params)
+            sample_prior_llh, proposal_prior_llh = self.prior.logpdf(sample_params, proposal_params)
             # Save
             self.samples.save_sample_prior_llh(sample_prior_llh)
             self.samples.save_proposal_prior_llh(proposal_prior_llh)
