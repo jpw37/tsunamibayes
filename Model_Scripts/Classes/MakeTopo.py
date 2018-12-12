d7ce571bab2d862fc6c266a07026cc8ea9463acd
Codac <codac@live.com>
2018-12-11 22:41:49 -0700

    Testing

--- Model_Scripts/Classes/MakeTopo.py
+++ Model_Scripts/Classes/MakeTopo.py
@@ -85,7 +85,7 @@ def make_dtopo(params, makeplots=False):
         #y = numpy.linspace(-40, -30, 100)
         times = [1.]
 
-        with open('model_bounds.txt') as json_file:
+        with open('../Data/model_bounds.txt') as json_file:
             model_bounds = json.load(json_file)
 
         xlower = model_bounds['xlower']
