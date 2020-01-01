fixed strike prior calculation

Model_Scripts/Scenarios/1852mag/Classes/Fault.py
@@ -53,11 +53,11 @@ class Fault:
         Returns -1 if on the hanging block, 1 if on the foot block.
         """
         if 0 <= (strike+45)%360 < 90:
-            return np.sign(lon-fault_lon)
+            return np.sign((lon-fault_lon+180)%360-180)
         elif 90 <= (strike+45)%360 < 180:
             return -np.sign(lat-fault_lat)
         elif 180 <= (strike+45)%360 < 270:
-            return -np.sign(lon-fault_lon)
+            return -np.sign((lon-fault_lon+180)%360-180)
         else:
             return np.sign(lat-fault_lat)
 

Model_Scripts/Scenarios/1852mag/Classes/Prior.py
@@ -32,12 +32,16 @@ class LatLonStrikePrior:
     def logpdf(self,lat,lon,strike):
         """Evaluates the logpdf of the prior"""
         distance,mean_strike = self.fault.distance_strike(lat,lon)
-        return self.latlon.logpdf(distance) + self.strike.logpdf((strike-mean_strike)%360)
+        strikediff = strike - mean_strike
+        strikediff = np.abs((strikediff + 180) % 360 - 180)
+        return self.latlon.logpdf(distance) + self.strike.logpdf(strikediff)
 
     def pdf(self,lat,lon,strike):
         """Evaluates the pdf of the prior"""
         distance,mean_strike = self.fault.distance_strike(lat,lon)
-        return self.latlon.pdf(distance)*self.strike.pdf((strike-mean_strike)%360)
+        strikediff = strike - mean_strike
+        strikediff = np.abs((strikediff + 180) % 360 - 180)
+        return self.latlon.pdf(distance)*self.strike.pdf(strikediff)
 
     def rvs(self):
         """Return a random point on the fault"""