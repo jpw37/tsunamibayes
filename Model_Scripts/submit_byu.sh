2a9665900331fea57b96cbdce7d753b9a7dafbe4
Jared Whitehead <jpw37@login02.rc.byu.edu>
2020-02-24 21:51:19 -0700

    messing around still

--- Model_Scripts/submit_byu.sh
+++ Model_Scripts/submit_byu.sh
@@ -4,7 +4,7 @@
 ## Submission flags (Customize for VT/BYU) ##
 #SBATCH --nodes=1
 #SBATCH --ntasks=12
-#SBATCH --time=48:00:00
+#SBATCH --time=00:20:00
 #SBATCH --mem-per-cpu=4096M #memory requirement
 #SBATCH --mail-user=whitehead@mathematics.byu.edu   # email address
 #SBATCH --mail-type=END
@@ -73,10 +73,9 @@ echo "LOG: $( date ): MCMC run start"    | tee -a $logfile
 python Main.py             \
     --scen    1852mag      \
     --mcmc    random_walk  \
-    --nsamp   500         \
+    --nsamp   50         \
     --rundir  $rundir      \
     --init manual         \
-    --adjoint             \
     | tee -a $logfile
 
 #    --resdir /fslhome/jpw37/fsl_groups/fslg_tsunami/compute/runs/34319122_m8 \
