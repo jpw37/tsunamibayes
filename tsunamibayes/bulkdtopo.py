6234876ec98a38e79a95011b67cfcd69462ff001
Hayden Ringer <hjrrockies@gmail.com>
2020-07-02 12:59:14 -0600

    adding verbosity to bulkdtopo.py

--- tsunamibayes/bulkdtopo.py
+++ tsunamibayes/bulkdtopo.py
@@ -6,9 +6,10 @@ import pandas as pd
 def load_model_params(path):
     return pd.read_csv(path,index_col=0)
 
-def compute_subfault_params(fault,model_params):
+def compute_subfault_params(fault,model_params,verbose=False):
     subfault_params = list()
-    for _,row in model_params.iterrows():
+    for idx,row in model_params.iterrows():
+        if verbose: print(idx,flush=True)
         subfault_params.append(fault.subfault_split(row['latitude'],
                                                     row['longitude'],
                                                     row['length'],
@@ -18,16 +19,18 @@ def compute_subfault_params(fault,model_params):
                                                     row['rake']))
     return subfault_params
 
-def make_faults(subfault_params,bounds):
+def make_faults(subfault_params,bounds,verbose=False):
     faults = list()
-    for df in subfault_params:
+    for idx,df in enumerate(subfault_params):
+        if verbose: print(idx,flush=True)
         faults.append(tb.maketopo.make_fault_dtopo(df,bounds))
 
     return faults
 
-def get_dtopos(faults):
+def get_dtopos(faults,verbose=False):
     x,y = faults[0].dtopo.x,faults[0].dtopo.y
     dtopos = list()
-    for fault in faults:
+    for idx,fault in enumerate(faults):
+        if verbose: print(idx,flush=True)
         dtopos.append(fault.dtopo.dZ[0])
     return np.array(dtopos),x,y
