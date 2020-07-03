f8c14e0d35b835bd795d8ecbd8562dfbc751c5c9
Hayden Ringer <hjrrockies@gmail.com>
2020-07-03 17:23:01 -0600

    fixes to sequential and bulktopo

--- tsunamibayes/bulkdtopo.py
+++ tsunamibayes/bulkdtopo.py
@@ -1,36 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import tsunamibayes as tb
 import pandas as pd
+from .maketopo import make_fault_dtopo
 
-def load_model_params(path):
-    return pd.read_csv(path,index_col=0)
-
-def compute_subfault_params(fault,model_params,verbose=False):
-    subfault_params = list()
+def compute_dtopos(fault,model_params,verbose=False):
+    model_params = model_params.reset_index()
     for idx,row in model_params.iterrows():
+        subfault_params = fault.subfault_split(row['latitude'],
+                                               row['longitude'],
+                                               row['length'],
+                                               row['width'],
+                                               row['slip'],
+                                               row['depth_offset'],
+                                               row['rake'])
+       
         if verbose: print(idx,flush=True)
-        subfault_params.append(fault.subfault_split(row['latitude'],
-                                                    row['longitude'],
-                                                    row['length'],
-                                                    row['width'],
-                                                    row['slip'],
-                                                    row['depth_offset'],
-                                                    row['rake']))
-    return subfault_params
-
-def make_faults(subfault_params,bounds,verbose=False):
-    faults = list()
-    for idx,df in enumerate(subfault_params):
-        if verbose: print(idx,flush=True)
-        faults.append(tb.maketopo.make_fault_dtopo(df,bounds))
-
-    return faults
-
-def get_dtopos(faults,verbose=False):
-    x,y = faults[0].dtopo.x,faults[0].dtopo.y
-    dtopos = list()
-    for idx,fault in enumerate(faults):
-        if verbose: print(idx,flush=True)
-        dtopos.append(fault.dtopo.dZ[0])
-    return np.array(dtopos),x,y
+        clawfault = make_fault_dtopo(subfault_params,fault.bounds)
+        dtopo = clawfault.dtopo.dZ[0].copy()
+        if idx == 0:
+            dtopos = np.empty((len(model_params),*dtopo.shape))
+            x,y = clawfault.dtopo.x.copy(),clawfault.dtopo.y.copy()
+        dtopos[idx] = dtopo
+    return dtopos,x,y
--- tsunamibayes/sequential.py
+++ tsunamibayes/sequential.py
@@ -4,12 +4,18 @@ import numpy as np
 import pandas as pd
 
 def resample(output_dirs, overwrite=False,  verbose=False):
-    samples = [pd.read_csv(dir_+"output/samples.csv", index_col=0) for dir_ in output_dirs]
-    final_samples = [df.iloc[-1] for df in samples]
-    model_params = [pd.read_csv(dir_+"output/model_params.csv", index_col=0) for dir_ in output_dirs]
-    model_output = [pd.read_csv(dir_+"output/model_output.csv", index_col=0) for dir_ in output_dirs]
-    bayes_data = [pd.read_csv(dir_+"output/bayes_data.csv", index_col=0) for dir_ in output_dirs]
-    debug = [pd.read_csv(dir_+"output/debug.csv", index_col=0) for dir_ in output_dirs]
+    samples = [pd.read_csv(dir_+"output/samples.csv", index_col=0).reset_index(drop=True) 
+               for dir_ in output_dirs]
+    final_idx = [df.index[-1] for df in samples]
+    model_params = [pd.read_csv(dir_+"output/model_params.csv", index_col=0).reset_index(drop=True)
+                    for dir_ in output_dirs]
+    model_output = [pd.read_csv(dir_+"output/model_output.csv", index_col=0).reset_index(drop=True) 
+                    for dir_ in output_dirs]
+    bayes_data = [pd.read_csv(dir_+"output/bayes_data.csv", index_col=0).reset_index(drop=True) 
+                  for dir_ in output_dirs]
+    debug = [pd.read_csv(dir_+"output/debug.csv", index_col=0).reset_index(drop=True) 
+             for dir_ in output_dirs]
+    final_debug_idx = [df.index[-1] for df in debug]
 
     p = np.exp(np.array([df['posterior_logpdf'].iloc[-1] for df in bayes_data]))
     p /= p.sum()
@@ -19,28 +25,61 @@ def resample(output_dirs, overwrite=False,  verbose=False):
         for i,dir_ in enumerate(output_dirs):
             print(dir_+": "+str(p[i]))
         print("\nResample results:\n-------------------")
-
+    
     zipped = zip(samples,model_params,model_output,
                  bayes_data,debug,resample_idx,output_dirs)
-    p_cols = ['p_'+col for col in samples[0].columns]
+    
     for df_s,df_mp,df_mo,df_bd,df_d,idx,dir_ in zipped:
         if verbose:
             print("Chain at {}:\n".format(dir_))
             print("Resampled from {}".format(output_dirs[idx]))
-            print(final_samples[idx],"\n")
-            df_d.loc[len(df_d),df_s.columns] = df_s.iloc[-1]
-            df_d.loc[len(df_d),p_cols] = final_samples[idx].rename(lambda x:'p_'+x)
-            df_d.loc[len(df_d),'accepted'] = 1
-            df_s.loc[len(df_s)] = final_samples[idx]
-            df_mp.loc[len(df_mp)] = model_params[idx].iloc[-1]
-            df_mo.loc[len(df_mo)] = model_output[idx].iloc[-1]
-            df_bd.loc[len(df_bd)] = bayes_data[idx].iloc[-1]
+            print(samples[idx].loc[final_idx[idx]],"\n")
+       
         if overwrite:
+            df_s.loc[len(df_s)] = samples[idx].loc[final_idx[idx]]
+            df_mp.loc[len(df_mp)] = model_params[idx].loc[final_idx[idx]]
+            df_mo.loc[len(df_mo)] = model_output[idx].loc[final_idx[idx]]
+            df_bd.loc[len(df_bd)] = bayes_data[idx].loc[final_idx[idx]]
+
+            metro_hastings_data = pd.Series({'alpha':1,'accepted':1,
+                                             'acceptance_rate':np.nan})
+            df_d.loc[len(df_d)] = gen_debug_row(df_s.iloc[-2],df_s.iloc[-1],
+                                                df_mp.iloc[-2],df_mp.iloc[-1],
+                                                df_bd.iloc[-2],df_bd.iloc[-1],
+                                                metro_hastings_data)
             df_s.to_csv(dir_+"output/samples.csv")
             df_mp.to_csv(dir_+"output/model_params.csv")
             df_mo.to_csv(dir_+"output/model_output.csv")
             df_bd.to_csv(dir_+"output/bayes_data.csv")
             df_d.to_csv(dir_+"output/debug.csv")
+
+def gen_debug_row(sample,proposal,sample_model_params,proposal_model_params,
+                      sample_bayes,proposal_bayes,metro_hastings_data):
+        """Create a Pandas Series object with the given data that is desired to
+        be kept in the debug output.
+
+        Parameters
+        ----------
+        # TODO: EITHER USE **kwargs OR A BUNCH OF SPECIFIC PARAMETERS?
+
+        Returns
+        -------
+        Pandas.Series
+            Row for the debug Dataframe
+        """
+        proposal = pd.Series(proposal).rename(lambda x:'p_'+x)
+        sample_model_params = pd.Series(sample_model_params).rename(
+                              lambda x:'m_'+x if x in sample.index else x)
+        proposal_model_params = pd.Series(proposal_model_params).rename(
+                                lambda x:'p_m_'+x if x in sample.index else 'p_'+x)
+        proposal_bayes = pd.Series(proposal_bayes).rename(lambda x:'p_'+x)
+        return pd.concat((sample,
+                          proposal,
+                          sample_model_params,
+                          proposal_model_params,
+                          sample_bayes,
+                          proposal_bayes,
+                          metro_hastings_data))
 
 if __name__ == "__main__":
     from sys import argv
