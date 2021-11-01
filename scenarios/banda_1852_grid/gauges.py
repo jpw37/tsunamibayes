aeb8f442e91e7f375d542d1459cbef758cb0876a
Taylor Paskett <taylor.paskett@gmail.com>
2021-11-01 10:11:04 -0600

    Fix gauges to use length-one lists for lat,lon

--- scenarios/banda_1852_grid/gauges.py
+++ scenarios/banda_1852_grid/gauges.py
@@ -3,7 +3,7 @@ import scipy.stats as stats
 
 def build_gauges():
     """Creates gauge object for each observation point's data and appends each to a list.
-    
+
     Returns
     -------
     gauges : (list) of Gauge objects
@@ -15,8 +15,8 @@ def build_gauges():
     dists = dict()
     dists['height'] = stats.norm(loc=3,scale=0.8)
     gauge = Gauge(name,dists)
-    gauge.lat = -4.5175
-    gauge.lon = 129.775
+    gauge.lat = [-4.5175]
+    gauge.lon = [129.775]
     gauges.append(gauge)
 
     # Ambon
@@ -24,8 +24,8 @@ def build_gauges():
     dists = dict()
     dists['height'] = stats.norm(loc=1.8,scale=0.4)
     gauge = Gauge(name,dists)
-    gauge.lat = -3.691
-    gauge.lon = 128.178
+    gauge.lat = [-3.691]
+    gauge.lon = [128.178]
     gauges.append(gauge)
 
     # Banda Neira
@@ -35,8 +35,8 @@ def build_gauges():
     dists['height'] = stats.norm(loc=6.5,scale=1.5)
     dists['inundation'] = stats.norm(loc=185,scale=65)
     gauge = Gauge(name,dists)
-    gauge.lat = -4.5248
-    gauge.lon = 129.8965
+    gauge.lat = [-4.5248]
+    gauge.lon = [129.8965]
     gauge.beta = 4.253277987952933
     gauge.n = 0.06
     gauges.append(gauge)
@@ -46,8 +46,8 @@ def build_gauges():
     dists = dict()
     dists['height'] = stats.chi(df=1.01,loc=0.5,scale=1.5)
     gauge = Gauge(name,dists)
-    gauge.lat = -3.3815
-    gauge.lon = 127.113
+    gauge.lat = [-3.3815]
+    gauge.lon = [127.113]
     gauges.append(gauge)
 
     # Hulaliu
@@ -55,10 +55,10 @@ def build_gauges():
     dists = dict()
     dists['height'] = stats.chi(df=1.01,loc=0.5,scale=2.0)
     gauge = Gauge(name,dists)
-    gauge.lat = -3.543
-    gauge.lon = 128.557
+    gauge.lat = [-3.543]
+    gauge.lon = [128.557]
     gauges.append(gauge)
-    
+
     # Saparua
     name = 'Saparua'
     dists = dict()
@@ -66,8 +66,8 @@ def build_gauges():
     dists['height'] = stats.norm(loc=5,scale=1)
     dists['inundation'] = stats.norm(loc=125,scale=40)
     gauge = Gauge(name,dists)
-    gauge.lat = -3.576
-    gauge.lon = 128.657
+    gauge.lat = [-3.576]
+    gauge.lon = [128.657]
     gauge.beta = 1.1067189507222546
     gauge.n = 0.06
     gauges.append(gauge)
@@ -77,8 +77,8 @@ def build_gauges():
     dists = dict()
     dists['height'] = stats.norm(loc=3,scale=1)
     gauge = Gauge(name,dists)
-    gauge.lat = -3.501
-    gauge.lon = 128.562
+    gauge.lat = [-3.501]
+    gauge.lon = [128.562]
     gauges.append(gauge)
 
     # Ameth
@@ -86,8 +86,8 @@ def build_gauges():
     dists = dict()
     dists['height'] = stats.norm(loc=3,scale=1)
     gauge = Gauge(name,dists)
-    gauge.lat = -3.6455
-    gauge.lon = 128.807
+    gauge.lat = [-3.6455]
+    gauge.lon = [128.807]
     gauges.append(gauge)
 
     # Amahai
@@ -95,8 +95,8 @@ def build_gauges():
     dists = dict()
     dists['height'] = stats.norm(loc=3.5,scale=1)
     gauge = Gauge(name,dists)
-    gauge.lat = -3.338
-    gauge.lon = 128.921
+    gauge.lat = [-3.338]
+    gauge.lon = [128.921]
     gauges.append(gauge)
 
     return gauges
