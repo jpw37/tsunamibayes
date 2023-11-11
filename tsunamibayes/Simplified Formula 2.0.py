f0b61340ee405fc1222dffb576f6418a5594b25a
ashspenc <ashleyspencer13973@gmail.com>
2023-11-11 15:09:42 -0700

    Placed Height Model into a Class

--- tsunamibayes/Simplified Formula 2.0.py
+++ tsunamibayes/Simplified Formula 2.0.py
@@ -3,133 +3,137 @@ import numpy as np
 import sys
 from scipy.interpolate import RectBivariateSpline
 
+class HeightModel:
+    def __init__(self, slip, length, width, rake, dip, lat1, lon1, lat2, lon2, shortest_time_path):
+        # Initialize parameters as attributes
+        self.slip = slip
+        self.length = length
+        self.width = width
+        self.rake = rake
+        self.dip = dip
+        self.lat1 = lat1
+        self.lon1 = lon1
+        self.lat2 = lat2
+        self.lon2 = lon2
+        self.shortest_time_path = shortest_time_path
 
-def create_x():
-    start_value = 124.991666666667
-    step = 0.016666666667
-    array_length = 571
-    my_array = np.linspace(start_value, start_value + step * (array_length - 1), array_length)
-    return my_array
+        # Call the method to read the matrix once during initialization
+        # How are we going to read in the bathymetry data?? I don't know how to format this lol
+        self.matrix = self.read_file_into_matrix(sys.argv[1])
 
+    def create_x(self):
+        # Generate an array representing longitude coordinates in order to interpolate the depth at a specific latlon coordinate
+        start_value = 124.991666666667
+        step = 0.016666666667
+        array_length = 571
+        my_array = np.linspace(start_value, start_value + step * (array_length - 1), array_length)
+        return my_array
 
-def create_y():
-    start_value = -9.508333333333
-    step = 0.016666666667
-    array_length = 421
-    my_array = np.linspace(start_value, start_value + step * (array_length - 1), array_length)
-    return my_array
+    def create_y(self):
+        # Generate an array representing longitude coordinates in order to interpolate the depth at a specific latlon coordinate
+        start_value = -9.508333333333
+        step = 0.016666666667
+        array_length = 421
+        my_array = np.linspace(start_value, start_value + step * (array_length - 1), array_length)
+        return my_array
 
+    def make_matrix(self, lines):
+        # Convert a list of strings into a matrix of integers
+        matrix = []
+        for line in lines:
+            line = line.split()
+            new_line = [int(num) for num in line]
+            matrix.append(new_line)
+        return matrix[::-1]
 
-def make_matrix(lines):
-    # takes a list of strings and returns a list of lists of integers
-    matrix = []
-    for line in lines:
-        line = line.split()
-        new_line = []
-        for num in line:
-            num = int(num)
-            new_line.append(num)
-        matrix.append(new_line)
-    return matrix[::-1]
+    def readlines(self, filename):
+        with open(filename) as file:
+            return file.readlines()
 
+    def writelines(self, filename, content):
+        with open(filename, "w") as file:
+            file.writelines(content)
 
-def readlines(filename):
-    with open(filename) as file:
-        return file.readlines()
+    def read_file_into_matrix(self, infile):
+        lines = self.readlines(infile)
+        matrix = self.make_matrix(lines)
+        return np.array(matrix).T
 
+    def get_depth(self, lon, lat):
+        # Interpolate depth at a given longitude and latitude using the bathymetry matrix
+        x = self.create_x()
+        y = self.create_y()
+        z = np.array(self.matrix)
+        interp_function = RectBivariateSpline(x, y, z)
+        interp_value = interp_function(lon, lat)
+        return interp_value[0][0]
 
-def main(infile):
-    lines = readlines(infile)
-    matrix = make_matrix(lines)
-    return np.array(matrix).T
+    def depth_at_source(self):
+        # Calculate the depth at the source coordinates(lat1, lon1)
+        depth = self.get_depth(lon1, lat1)
+        return abs(depth)
 
+    def alpha(self):
+        a = (1 - (dip/180)) * math.sin(np.deg2rad(dip)) * abs(math.sin(np.deg2rad(rake)))
+        return a
 
-def get_depth(lon, lat):
-    matrix = main(sys.argv[1])
-    x = create_x()
-    y = create_y()
-    z = np.array(matrix)
-    interp_function = RectBivariateSpline(x, y, z)
-    x_val = lon
-    y_val = lat
-    interp_value = interp_function(x_val, y_val)
-    return interp_value[0][0]
+    def initial_tsunami_amplitude(self):
+        a_naught = (self.alpha() * self.slip) / (math.cosh(np.deg2rad((4 * math.pi * self.depth_at_source())/(self.width + self.length))))
+        return a_naught
 
-
-def depth_at_source():
-    depth = get_depth(lon1, lat1)
-    return abs(depth)
-
-
-def alpha():
-    a = (1 - (dip/180)) * math.sin(np.deg2rad(dip)) * abs(math.sin(np.deg2rad(rake)))
-    return a
-
-
-def initial_tsunami_amplitude():
-    a_naught = (alpha() * slip) / (math.cosh(np.deg2rad((4 * math.pi * depth_at_source())/(width + length))))
-    return a_naught
-
-
-def mean_depth():
-    depth_lst = []
-    for lat_lon in shortest_time_path:
-        lat, lon = lat_lon
-        depth_lst.append(get_depth(lon, lat))
+    def mean_depth(self):
+        # Compute the mean depth along the shortest time path
+        depth_lst = [self.get_depth(lat_lon[1], lat_lon[0]) for lat_lon in self.shortest_time_path]
         return abs(sum(depth_lst) / len(depth_lst))
 
+    def psi(self):
+        p = .5 + (0.575 * math.exp(-.0175 * (self.length / self.mean_depth())))
+        return p
 
-def psi():
-    p = .5 + (0.575 * math.exp(-.0175 * (length / mean_depth())))
-    return p
+    def haversine(self):
+        """Computes great-circle distance between sets of lat-lon coordinates on a
+        sphere with radius R.
 
+        Parameters
+        ----------
+        lat1 : float -or- array_like of floats
+            The coordinate or ndarray of coordinates associated with the initial
+            latitude.
+        lon1 : float -or- array_like of floats
+            The coordinate or ndarray of coordinates associated with the initial
+            longitude.
+        lat2 : float -or- array_like of floats
+            The coordinate or ndarray of coordinates associated with the terminal
+            latitude.
+        lon2 : float -or- array_like of floats
+            The coordinate or ndarray of coordinates associated with the terminal
+            latitude.
 
-def haversine():
-    """Computes great-circle distance between sets of lat-lon coordinates on a
-    sphere with radius R.
+        Returns
+        -------
+        distance : float -or- ndarray of floats
+            The computed distance (in meters) between the given point(s).
+            Returns object of the same dimension as the lat/lon parameters.
+        """
+        # Earth's radius, in meters
+        R = 6.3781e6
 
-    Parameters
-    ----------
-    lat1 : float -or- array_like of floats
-        The coordinate or ndarray of coordinates associated with the initial
-        latitude.
-    lon1 : float -or- array_like of floats
-        The coordinate or ndarray of coordinates associated with the initial
-        longitude.
-    lat2 : float -or- array_like of floats
-        The coordinate or ndarray of coordinates associated with the terminal
-        latitude.
-    lon2 : float -or- array_like of floats
-        The coordinate or ndarray of coordinates associated with the terminal
-        latitude.
+        phi1,phi2 = np.deg2rad(self.lat1),np.deg2rad(self.lat2)
+        lam1,lam2 = np.deg2rad(self.lon1),np.deg2rad(self.lon2)
+        term = (np.sin(.5 * (phi2 - phi1))**2 + np.cos(phi1) * np.cos(phi2) * np.sin(.5 * (lam2 - lam1))**2)
+        return 2*R*np.arcsin(np.sqrt(term))
 
-    Returns
-    -------
-    distance : float -or- ndarray of floats
-        The computed distance (in meters) between the given point(s).
-        Returns object of the same dimension as the lat/lon parameters.
-    """
-    # Earth's radius, in meters
-    R = 6.3781e6
+    def propagation_loss(self):
+        p = (1 + ((2 * self.haversine()) / self.length)) ** self.psi()
+        return p
 
-    phi1,phi2 = np.deg2rad(lat1),np.deg2rad(lat2)
-    lam1,lam2 = np.deg2rad(lon1),np.deg2rad(lon2)
-    term = (np.sin(.5 * (phi2 - phi1))**2 + np.cos(phi1) * np.cos(phi2) * np.sin(.5 * (lam2 - lam1))**2)
-    return 2*R*np.arcsin(np.sqrt(term))
+    def shoaling_correction(self):
+        s_l = (self.depth_at_source()/self.mean_depth()) ** .25
+        return s_l
 
-def propagation_loss():
-    p = (1 + ((2 * haversine()) / length)) ** psi()
-    return p
-
-
-def shoaling_correction():
-    s_l = (depth_at_source()/mean_depth()) ** .25
-    return s_l
-
-
-def wave_height():
-    a_s_r = initial_tsunami_amplitude() * propagation_loss() * shoaling_correction()
-    return a_s_r
+    def wave_height(self):
+        a_s_r = self.initial_tsunami_amplitude() * self.propagation_loss() * self.shoaling_correction()
+        return a_s_r
 
 
 if __name__ == '__main__':
@@ -194,5 +198,7 @@ if __name__ == '__main__':
            (129.77500000009601, -4.458333333232001), (129.77500000009601, -4.474999999899),
            (129.77500000009601, -4.491666666566), (129.77500000009601, -4.508333333233001),
            (129.77500000009601, -4.5249999999)]
-    print(wave_height())
+    heightmodel = HeightModel(slip, length, width, rake, dip, lat1, lon1, lat2, lon2, shortest_time_path)
+
+    print(heightmodel.wave_height())
 
